9c5c5cee0be65ce6e1d28aeccd1733418d3c9598
Efrain Garcia <edx1803@gmail.com>
2017-10-24 09:33:18 -0600

    Agregando Pruebas de AJAX

---
+++ Controllers/AJAXConceptController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TiendaVirtual.Controllers
+{
+    public class AJAXConceptController : Controller
+    {
+        // GET: AJAXConcept
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
